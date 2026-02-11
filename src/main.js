052bf73334451c6af330a425bbbea713a495bd54
Toshiyuki Masui <masui@pitecan.com>
2026-02-11 13:05:12 +0900

    ...

--- src/main.js
+++ src/main.js
@@ -2,37 +2,38 @@
 //import { initMap } from "./map";
 
 async function main() {
-  console.log("Mapfeel starting...");
-
-  //const map = initMap();
-  initMap();
-
-  // ここに将来:
-  // Scrapboxからフェッチする
-  // const images = extractGyazo(...)
-  // addImagesToMap(map, images)
-
-  function getProject() {
-    return location.pathname.replace(/^\//, "");
-  }
-  
-  async function load() {
-    const project = getProject();
+    console.log("Mapfeel starting...");
     
-    if (!project) {
-      alert("URLにプロジェクト名を指定してください");
-      return;
+    //const map = initMap();
+    initMap();
+    
+    // ここに将来:
+    // Scrapboxからフェッチする
+    // const images = extractGyazo(...)
+    // addImagesToMap(map, images)
+    
+    function getProject() {
+	alert(location.pathname)
+	return location.pathname.replace(/^\//, "");
     }
     
-    const res = await fetch(`/api/scrapbox/${project}`);
-    const data = await res.json();
+    async function load() {
+	const project = getProject();
+	
+	if (!project) {
+	    alert("URLにプロジェクト名を指定してください");
+	    return;
+	}
+	
+	const res = await fetch(`/api/scrapbox/${project}`);
+	const data = await res.json();
+	
+	console.log(data);
+	
+	// 既存のマーカー処理につなげる
+    }
     
-    console.log(data);
-    
-    // 既存のマーカー処理につなげる
-  }
-  
-  load();
+    load();
 }
 
 main();
