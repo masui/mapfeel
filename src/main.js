013336943bfc887e3ff3d3db3f7ccb49c7032e73
Toshiyuki Masui <masui@pitecan.com>
2026-02-13 13:24:15 +0900

    ...

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import { initMap } from "/src/map.js";
-import { getData } from "/src/data.js";
-
-export async function main(project) {
-    //const map = initMap();
-    //initMap();
-    
-    async function load(project) {
-	// ScrapboxからMapfeel用データを取得
-	const validdata = await getData(project)
-
-	return validdata;
-    }
-    
-    return load(project);
-}
+//import { initMap } from "/src/map.js";
+//import { getData } from "/src/data.js";
+//
+//export async function main(project) {
+//    //const map = initMap();
+//    //initMap();
+//    
+//    async function load(project) {
+//	// ScrapboxからMapfeel用データを取得
+//	const validdata = await getData(project)
+//
+//	return validdata;
+//    }
+//    
+//    return load(project);
+//}
 
 
