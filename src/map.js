9f89f85cba9240006261e2c72ba3ecd3a7fe34a7
Toshiyuki Masui <masui@pitecan.com>
2026-02-13 16:28:33 +0900

    ...

--- src/map.js
+++ src/map.js
@@ -11,6 +11,16 @@ export function initMap() {
   return map;
 }
 
+export function distance(lat1, lng1, lat2, lng2) {
+    const R = Math.PI / 180;
+    lat1 *= R; // ラジアンに変換
+    lng1 *= R;
+    lat2 *= R;
+    lng2 *= R;
+    return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+}
+
+
 export function getCenter(){
 }
 
