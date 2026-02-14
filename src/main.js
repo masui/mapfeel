5cdc707e93d8b26e06ee15f419ea63aacc9ee17e
Toshiyuki Masui <masui@pitecan.com>
2026-02-14 16:09:40 +0900

    ...

--- src/main.js
+++ src/main.js
@@ -85,6 +85,10 @@ map.on('moveend', function () {
         return a.distance > b.distance ? 1 : -1;
     })
 
+    $('#img0').src = data[0].image
+    $('#img1').src = data[1].image
+    $('#img2').src = data[2].image
+
     showlist()
 });
 
