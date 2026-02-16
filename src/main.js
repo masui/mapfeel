5e9d6ca1405fcea1844ffa2d891834c7a065c632
Toshiyuki Masui <masui@pitecan.com>
2026-02-16 09:51:14 +0900

    ...

--- src/main.js
+++ src/main.js
@@ -151,7 +151,11 @@ function showlist(list){
 	    curpos.lat = e.target.lat
 	    curpos.lng = e.target.lng
 	    map.flyTo([curpos.lat, curpos.lng], map.getZoom())
-	    setImages(400); // 拡大表示
+	    map.once('moveend', () => {
+		console.log('flyTo 完了');
+		setImages(400); // 拡大表示
+	    });
+	    //setImages(400); // 拡大表示
 	})
     }
 }
