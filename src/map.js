014a04e64d6ca27ecca99094c27ee759ca88a36c
Toshiyuki Masui <masui@pitecan.com>
2026-02-20 20:04:19 +0900

    ...

--- src/map.js
+++ src/map.js
@@ -1,10 +1,10 @@
 var map = null;
 
 export function initMap(lat, lng) {
-    //map = L.map("map").setView([35.319, 139.550], 13);
     // キー操作を全部ディスエーブルしてみる
     map = L.map("map", { keyboard: false })
-	.setView([lat, lng], 13)
+    
+    map.setView([lat, lng], 13)
 
     L.tileLayer(
 	"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -22,37 +22,3 @@ export function distance(lat1, lng1, lat2, lng2) {
     lng2 *= R;
     return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
 }
-
-/*
-import L from "leaflet";
-
-export function initMap() {
-  const map = L.map("map").setView([35.681, 139.767], 13);
-
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution: "© OpenStreetMap",
-  }).addTo(map);
-
-  return map;
-}
-
-export function addImageMarker(
-  map: L.Map,
-  lat: number,
-  lng: number,
-  imageUrl: string,
-  title: string
-) {
-  const marker = L.marker([lat, lng]).addTo(map);
-
-  marker.bindPopup(`
-    <div>
-      <h3>${title}</h3>
-      <img src="${imageUrl}" style="width:200px;">
-    </div>
-  `);
-
-  return marker;
-}
-*/
